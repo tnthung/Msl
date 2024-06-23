cf3391e3bbb1153bd87498cc88b21cbd2da0fc82
TNThung <hung11112002@gmail.com>
2024-06-23 12:45:37 +0800

    [Add] Parsable trait

--- Mslc/parser/src/lib.rs
+++ Mslc/parser/src/lib.rs
@@ -1,1 +1,13 @@
 mod ast;
+
+
+use types::stream::*;
+
+
+pub trait Parsable: Sized {
+  fn parse(ts: &Stream<ast::Ast>) -> Option<(Self, Stream<ast::Ast>)>;
+
+  fn parse_in_place(ts: &mut Stream<ast::Ast>) -> Option<Self> {
+    Self::parse(ts).map(|(x, n)| { *ts = n; x })
+  }
+}
